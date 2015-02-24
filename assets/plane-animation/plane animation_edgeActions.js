4ea5785c709db4f82b59cc60e4045cb4445fc0f3
unknown <shaikh.saqib704@gmail.com>
2015-02-25 00:08:37 +0530

    Changes affected in gulf-graphic folder

--- assets/plane-animation/plane animation_edgeActions.js
+++
@@ -1,8 +0,0 @@
-
-(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
-//Edge symbol: 'stage'
-(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Text2}","click",function(sym,e){sym.play();});
-//Edge binding end
-})("stage");
-//Edge symbol end:'stage'
-})(jQuery,AdobeEdge,"EDGE-22157104");
